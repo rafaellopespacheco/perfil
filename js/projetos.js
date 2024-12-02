c43a9744a738174bc782b64ecc57d5e672e1befe
Rafael Lopes <rafael.pachecos2007@gmail.com>
2024-12-02 01:42:41 -0300

    Adicionado aba Projetos
    Foi adicionado conteudo na parte de projetos atraves de uma db json

---
+++ js/projetos.js
@@ -0,0 +1,23 @@
+// Função para carregar o changelog
+fetch('../db/projetos.json')
+   .then(response => response.json())
+   .then(data => {
+      const changelogList = document.getElementById('changelog-list');
+
+      data.forEach(item => {
+         const changelogItem = document.createElement('div');
+         changelogItem.classList.add('projetos-item');
+
+         const itemContent = `
+        <h3>${item.titulo}</h3>
+        <p class="data"><strong>Data:</strong> ${item.data}</p>
+        <p>${item.descricao}</p>
+        <img src="${item.imagem}" alt="Imagem da atualização" />
+        <a href="${item.link}" target="_blank" class="acessarprojeto">Acessar Projeto</a>
+      `;
+
+         changelogItem.innerHTML = itemContent;
+         changelogList.appendChild(changelogItem);
+      });
+   })
+   .catch(error => console.error('Erro ao carregar o changelog:', error));
