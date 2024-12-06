2f4c0b98084cb85515c829a4398848f4f2d5c6e2
Rafael Lopes <rafael.pachecos2007@gmail.com>
2024-12-06 01:10:02 -0300

    Adicionado versão em projetos e adicionado novo projeto

--- js/projetos.js
+++ js/projetos.js
@@ -11,6 +11,7 @@ fetch('../db/projetos.json')
          const itemContent = `
         <h3>${item.titulo}</h3>
         <p class="data"><strong>Data:</strong> ${item.data}</p>
+        <p class="data"><strong>Versão:</strong> ${item.versao}</p>
         <p>${item.descricao}</p>
         <img src="${item.imagem}" alt="Imagem da atualização" />
         <a href="${item.link}" target="_blank" class="acessarprojeto">Acessar Projeto</a>
