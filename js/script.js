1f162d15bc8ae9929ad96f8bf32c5e3730f0bbb7
Rafael Lopes <80158191+rafaellopespacheco@users.noreply.github.com>
2024-12-28 12:07:09 -0300

    fix #001

--- js/script.js
+++ js/script.js
@@ -1,7 +1,4 @@
-// Referências aos elementos
-const modal = document.getElementById('myModal');
-const openModalBtn = document.getElementById('openModalBtn');
-const closeBtn = document.getElementsByClassName('close-btn')[0];
+
 
 const myObserver = new IntersectionObserver((entries) => {
    entries.forEach( (entry) => {
@@ -15,21 +12,3 @@ const myObserver = new IntersectionObserver((entries) => {
 const elements = document.querySelectorAll('.hidden');
 
 elements.forEach( (element) => myObserver.observe(element))
-
-// Quando o usuário clica no botão, o modal será exibido
-openModalBtn.onclick = function () {
-   modal.style.display = 'block';
-}
-
-// Quando o usuário clica no X, o modal será fechado
-closeBtn.onclick = function () {
-   modal.style.display = 'none';
-}
-
-// Se o usuário clicar fora do modal, ele também será fechado
-window.onclick = function (event) {
-   if (event.target == modal) {
-      modal.style.display = 'none';
-   }
-}
-
